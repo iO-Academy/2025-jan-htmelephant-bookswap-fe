b7424d076bd8bd6cc0a0349cb32face9eb1b52a1
Jason Banks <jasonbankse@gmail.com>
2025-02-10 16:25:48 +0000

    nav added

---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+export default function Nav() {
+ return (
+    <nav className="">
+        <Link to="/">Home</Link>
+        <h1>Book Swap</h1>
+    </nav>
+
+
+ )
+}

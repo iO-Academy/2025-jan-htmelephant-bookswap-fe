36e35c30dfc2e87a4e332d950e13b9e1d5e1dab9
Jason Banks <jasonbankse@gmail.com>
2025-02-10 17:35:14 +0000

    story 1 -design

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,12 +1,11 @@
 export default function BookCard({title, author, genre, image, id}) {
     return (
         <div className="">
-            <h3 className="">{title}</h3>
-            <img src={image} key={id} />
-            <ul>
-                <li>Id: {id}</li>
-                <li>Genre: {genre}</li>
-                <li>Author: {author}</li>
+            <img src={image} />
+            <ul key={id}>
+                <li>{title}</li>
+                <li>{author}</li>
+                <li>{genre}</li>
             </ul>
         </div>
     )

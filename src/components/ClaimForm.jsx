c4ab7983b8726ce4c3d78b62985b220eb6b79d7f
BigDaveCoding <bigdavecoding@gmail.com>
2025-02-11 10:36:33 +0000

    using properties to determine whether form is displayed or claimed by 'x' is displayed using a ternary condition

--- src/components/ClaimForm.jsx
+++ src/components/ClaimForm.jsx
@@ -1,13 +1,21 @@
-export default function ClaimForm() {
+import { useState } from "react"
+
+export default function ClaimForm({claimedBy}) {
+
     return(
         <>
-        <form>
-            <label htmlFor="fname">First Name</label>
-            <input id="fname" type="text" name="fname"/>
-            <label htmlFor="email">Email</label>
-            <input id="email" type="email" name="email"/>
-            <input type="submit" value="Claim Book"/>
-        </form>
+            {claimedBy === null &&
+                <form>
+                    <label htmlFor="fname">First Name</label>
+                    <input id="fname" type="text" name="fname"/>
+                    <label htmlFor="email">Email</label>
+                    <input id="email" type="email" name="email"/>
+                    <input type="submit" value="Claim Book"/>
+                </form>
+            }
+
+            {claimedBy !== null && <p>Claimed by {claimedBy}</p>}
+            
         </>
     )
 }

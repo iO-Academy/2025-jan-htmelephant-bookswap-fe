5755eb14b6b71781815ae6107da68204a22a083d
Henry Shanahan <henry.shanahan@gmail.com>
2025-02-11 10:09:26 +0000

    styled single book and book grid

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,16 +1,17 @@
 import { Link } from "react-router-dom";
 
-export default function BookCard({title, author, genre, image, id}) {
-    return (
-        <div className="">
-            <Link to={`/books/${id}`}>
-                <img src={image} alt=""/>
-                <ul key={id}>
-                    <li>{title}</li>
-                    <li>{author}</li>
-                    <li>{genre}</li>
-                </ul>
-            </Link>
-        </div>
-    )
+export default function BookCard({ title, author, genre, image, id }) {
+  return (
+    <div className="">
+      <Link to={`/books/${id}`}>
+        <img src={image} alt="" />
+
+        <ul key={id}>
+          <li>{title}</li>
+          <li>{author}</li>
+          <li>{genre}</li>
+        </ul>
+      </Link>
+    </div>
+  );
 }

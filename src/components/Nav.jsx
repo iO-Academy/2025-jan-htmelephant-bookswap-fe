fb8e7a805537c821a4cbe280d5e433bbb4e2d395
Joshua Downton-Lewis <joshuadowntonlewis@outlook.com>
2025-02-11 14:20:04 +0000

    working on nav bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom";
-import ClaimedBookPage from "./ClaimedBookPage";
+import ClaimedBookPage from "./ClaimedBookNavLink";
 
 export default function Nav() {
   return (
+    
     <nav className="flex justify-between p-4 max-sm:flex-col max-sm:items-center">
       <Link to="/">Book Swap</Link>
+      <Link to="/claimed-books/">Claimed Books</Link>
+      
       <div className="flex gap-2">
         <ClaimedBookPage />
       </div>

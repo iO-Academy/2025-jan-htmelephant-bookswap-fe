c5942b166c7486b38f7f2fc2171c5e601c3a1b4a
BigDaveCoding <bigdavecoding@gmail.com>
2025-02-11 14:27:09 +0000

    added relevant links to nav bar. styled in grid with responsive design.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,13 +4,14 @@ import ClaimedBookPage from "./ClaimedBookNavLink";
 export default function Nav() {
   return (
     
-    <nav className="flex justify-between p-4 max-sm:flex-col max-sm:items-center">
-      <Link to="/">Book Swap</Link>
-      <Link to="/claimed-books/">Claimed Books</Link>
-      
-      <div className="flex gap-2">
-        <ClaimedBookPage />
+    <nav className="grid grid-cols-1 md:grid-cols-2  p-4 max-sm:flex-col max-sm:items-center items-center">
+      <Link className="text-center md:text-left" to="/">Book Swap</Link>
+
+      <div className="flex justify-center gap-4 text-center md:justify-end">
+        <Link to="/">Available Books</Link>
+        <Link to="/claimed-books/">Claimed Books</Link>
       </div>
+
     </nav>
   );
 }

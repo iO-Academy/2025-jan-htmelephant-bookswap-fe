1964e9810afc484380a52ffab0371cdd380314c2
BigDaveCoding <bigdavecoding@gmail.com>
2025-02-11 10:47:11 +0000

    created function placeholders

--- src/components/ClaimForm.jsx
+++ src/components/ClaimForm.jsx
@@ -2,14 +2,20 @@ import { useState } from "react"
 
 export default function ClaimForm({claimedBy}) {
 
+    function claimBook(){
+        
+    }
+
     return(
         <>
             {claimedBy === null &&
                 <form>
                     <label htmlFor="fname">First Name</label>
                     <input id="fname" type="text" name="fname"/>
+
                     <label htmlFor="email">Email</label>
                     <input id="email" type="email" name="email"/>
+
                     <input type="submit" value="Claim Book"/>
                 </form>
             }

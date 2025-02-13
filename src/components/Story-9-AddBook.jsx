1929346a0b647b90bec4abcc7e60573688f8e62d
BigDaveCoding <bigdavecoding@gmail.com>
2025-02-13 15:27:50 +0000

    story 9

---
+++ src/components/Story-9-AddBook.jsx
@@ -0,0 +1,158 @@
+import { useEffect, useState } from "react"
+
+export default function AddBook() {
+
+    //image url : https://placehold.co/600x400/EEE/31343C
+
+    const [genres, setGenres] = useState([])
+
+    const [title, setTitle] = useState("")
+    const [author, setAuthor] = useState("")
+    const [genre, setGenre] = useState(null)
+    const [year, setYear] = useState(NaN)
+    const [pageCount, setPageCount] = useState("")
+    const [imageURL, setImageURL] = useState("https://placehold.co/600x400/EEE/31343C") // declare placeholder image if user doesnt input an image
+    const [blurb, setBlurb] = useState("")
+    
+    const [postBody, setPostBody] = useState({})
+
+    const [errors, setErrors] = useState({})
+
+    const [success, setSuccess] = useState(false)
+
+    function fetchGenres(){
+        fetch('https://book-swap-api.dev.io-academy.uk/api/genres', {
+            mode: 'cors',
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                setGenres(data.data)
+                setGenre(0)
+        })
+    }
+
+    function submitBook(e) {
+        e.preventDefault()
+        console.log("submitting book")
+        try{
+            fetch('https://book-swap-api.dev.io-academy.uk/api/books', {
+                mode: 'cors',
+                method : 'POST',
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                },
+                body : JSON.stringify(postBody)
+            })
+                .then(res => {
+                    if (res.status == 201) {
+                        setSuccess(true)
+                    }
+                    return res.json()
+                })
+                .then(data => {
+                    console.log(data)
+                    if (data.errors){
+                        setErrors(data.errors)
+                    }
+            })
+
+        } catch(err) {
+            console.error(err)
+            console.log("catch in submit book running - error has occured")
+        }
+    }
+
+    function createPostBody(){
+        let body = {}
+
+        if (title) body.title = title
+        if (author) body.author = author
+        if (genre !== null) body.genre_id = genre
+        if (!isNaN(year)) body.year = year
+        if (blurb) body.blurb = blurb
+        if (imageURL) body.image = imageURL
+
+        setPostBody(body)
+
+    }
+
+    function handleInput(e){
+        switch (e.target.id) {
+            case "title":
+                setTitle(e.target.value)
+                break;
+            case "author":
+                setAuthor(e.target.value)
+                break;
+            case "genre":
+                setGenre(parseInt(e.target.value))
+                break;
+            case "year":
+                setYear(parseInt(e.target.value))
+                break;
+            case "page_count":
+                setPageCount(e.target.value)
+                break;
+            case "image":
+                setImageURL(e.target.value)
+            case "blurb":
+                setBlurb(e.target.value)
+            default:
+                break;
+        }
+    }
+
+    useEffect(fetchGenres, [])
+    useEffect(createPostBody, [title, author, genre, year, pageCount, imageURL, blurb])
+
+    return (
+        <>
+            <div className="flex justify-center">
+
+                {!success && 
+                    <form onSubmit={submitBook} className="grid grid-cols-1 w-10/12 gap-2 md:w-4/12">
+                        <label htmlFor="title">Title (Required)</label>
+                        <input className="border-1 p-2 rounded" onChange={handleInput} type="text" id="title" name="title" />
+                        {errors.title && title === "" && <p className="text-red-600">{errors.title[0]}</p>}
+
+                        <label htmlFor="author">Author (Required)</label>
+                        <input className="border-1 p-2 rounded" onChange={handleInput} type="text" id="author" name="author" />
+                        {errors.author && author === "" && <p className="text-red-600">{errors.author[0]}</p>}
+
+                        <label htmlFor="genre">Genre (Required)</label>
+                        <select className="border-1 p-2 rounded" onChange={handleInput} id="genre">
+                            <option value={0} defaultValue>Any</option>
+                            {genres.map((genre) => {
+                                return <option key={genre.id} value={genre.id}>{genre.name}</option>
+                            })}
+                        </select>
+                        {errors.genre_id && genre === 0 && <p className="text-red-500">Please Select A Genre</p>}
+
+                        <label htmlFor="year">Year</label>
+                        <input className="border-1 p-2 rounded" onChange={handleInput} type="text" id="year" name="year" />
+                        {errors.year && <p className="text-red-500">{errors.year[0]}</p>}
+
+                        <label htmlFor="page_count">Page Count</label>
+                        <input className="border-1 p-2 rounded" onChange={handleInput} type="text" id="page_count" name="page_count" />
+
+                        <label htmlFor="image">Image URL</label>
+                        <input className="border-1 p-2 rounded" onChange={handleInput} type="text" id="image" name="image" />
+
+                        <label htmlFor="blurb">Blurb</label>
+                        <textarea className="border-1 p-2 rounded" onChange={handleInput} id="blurb" name="blurb" />
+
+                        <input className="border-1 p-2 rounded" type="submit" value="Add Book" />
+                    </form>
+                }
+
+                {success && <p className="text-center text-2xl p-4">You Submitted a book! <br /> Thank You!</p>}
+
+            </div>
+        </>
+    )
+}

009c11361c4d077e846c22ac7204bf705e5b9290
BigDaveCoding <bigdavecoding@gmail.com>
2025-02-13 15:15:10 +0000

    added another link. changed styling of parent container to a grid.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,9 +9,12 @@ export default function Nav() {
         Book Swap
       </Link>
 
-      <div className="flex justify-center gap-4 text-center md:justify-end">
+      <div className="grid grid-cols-2 justify-center gap-4 text-center md:justify-end md:grid-cols-3">
         <DisplayedBooksLink link="/" text="Available Books" />
         <DisplayedBooksLink link="/claimed-books/" text="Claimed Books" />
+        <div className=" flex col-span-2 justify-center md:col-span-1">
+          <DisplayedBooksLink className="w-6/12 md:w-full" link="/addbook" text="Add Book" />
+        </div>
       </div>
     </nav>
   );

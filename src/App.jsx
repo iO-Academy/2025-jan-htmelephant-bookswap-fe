017e4db2c4c4516e302840d01d435658d86513d7
BigDaveCoding <bigdavecoding@gmail.com>
2025-02-13 15:14:21 +0000

    importing AddBook. Created route to AddBook component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Nav from "./components/Nav";
 import BookGrid from "./components/BookGrid";
 import DisplaySingleBook from "./components/DisplaySingleBook";
+import AddBook from "./components/Story-9-AddBook";
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
         <Route path="/books/:id" element={<DisplaySingleBook />} />
         <Route path="/claimed-books/" element={<BookGrid claimed={1} />} />
         <Route path="*" element={<h1>Not Found</h1>} />
+        <Route path="/addbook" element={<AddBook />} />
       </Routes>
     </BrowserRouter>
   );

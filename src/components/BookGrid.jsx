27590fc2c9790cba54ed3e187aa2863673bbde83
Jason Banks <jasonbankse@gmail.com>
2025-02-14 11:18:40 +0000

    search function

--- src/components/BookGrid.jsx
+++ src/components/BookGrid.jsx
@@ -2,30 +2,60 @@ import { useEffect, useState } from "react";
 import BookCard from "./BookCard";
 export default function BookGrid({ claimed }) {
   const [books, setBooks] = useState([]);
-  function getBooks() {
-    fetch(`https://book-swap-api.dev.io-academy.uk/api/books?claimed=${claimed}`)
+  const [search, setSearch] = useState("");
+  function getBooks(searching) {
+    let url = "https://book-swap-api.dev.io-academy.uk/api/books";
+
+    if (searching) {
+      url += `?search=${search}`;
+    } else {
+      url += `?claimed=${claimed}`;
+    }
+    fetch(url)
       .then((res) => res.json())
       .then((bookInfo) => {
         setBooks(bookInfo.data);
+        console.log(bookInfo)
       });
   }
 
-  useEffect(getBooks, [claimed]);
+  function updateBooks() {
+    getBooks(search);
+}
+useEffect(updateBooks, [search, claimed]);
 
   return (
-    <div className="grid grid-cols-1 gap-4 p-5 md:grid-cols-2 md:gap-6 md:p-10 lg:grid-cols-3">
-      {books.map(function (book) {
-        return (
-          <BookCard
-            key={book.id}
-            title={book.title}
-            image={book.image}
-            genre={book.genre.name}
-            author={book.author}
-            id={book.id}
-          />
-        );
-      })}
+    <div className="px-10 py-4">
+      <div className="mb-4">
+        <label htmlFor="search" className="mr-2 font-medium">
+          Search Books:
+        </label>
+        <input
+          type="text"
+          id="search"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          className="border p-1 rounded"
+          placeholder="Search books..."
+        />
+      </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {books.length > 0 ? (
+          books.map((book) => (
+            <BookCard
+              key={book.id}
+              title={book.title}
+              image={book.image}
+              genre={book.genre.name}
+              author={book.author}
+              id={book.id}
+            />
+          ))
+        ) : (
+          <p className="text-center col-span-full">No books found</p>
+        )}
+      </div>
     </div>
   );
 }
